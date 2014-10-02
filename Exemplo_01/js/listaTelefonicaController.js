1472b18568df9818ce3de502d91ed666c9b2678c
gustavobergamim <gsbergamim@gmail.com>
2014-10-02 19:58:47 -0300

    Validações e filtros
    Adicionadas validações e filtros do Angular

--- Exemplo_01/js/listaTelefonicaController.js
+++ Exemplo_01/js/listaTelefonicaController.js
@@ -1,14 +1,35 @@
 var app = angular.module('listaTelefonica', []);
 
-app.controller('ListaTelefonicaController', function($scope){
+app.controller('ListaTelefonicaController', ['$scope', '$interval', function ($scope, $interval) {
 	
 	$scope.titulo = 'Lista Telefônica';
-	$scope.contatos = [
-		{ nome: 'Teste 1', telefone: '(00) 1234-5678', selecionado: false, operadora:{ id:1, nome: 'VIVO' } },
-		{ nome: 'Teste 2', telefone: '(00) 1234-5678', selecionado: false, operadora:{ id:1, nome: 'VIVO' } },
-		{ nome: 'Teste 3', telefone: '(00) 1234-5678', selecionado: false, operadora:{ id:1, nome: 'VIVO' } },
-		{ nome: 'Teste 4', telefone: '(00) 1234-5678', selecionado: false, operadora:{ id:1, nome: 'VIVO' } }
-	];
+	$scope.agora = new Date();
+	$scope.ordenacao = {
+		campo: 'contato.nome',
+		direcao: false
+	};
+
+	$scope.paginacao = {
+		totalPaginas: 1,
+		qtdeRegistros: 5
+	};
+
+	$scope.controlarPaginacao = function (dados) {
+		return $scope.paginacao.totalPaginas * $scope.paginacao.qtdeRegistros;
+	};
+
+	$scope.fimDaLista = function () {
+		return $scope.paginacao.totalPaginas < ($scope.contatos.length / $scope.paginacao.qtdeRegistros);
+	};
+
+	$scope.maisContatos = function () {
+		$scope.paginacao.totalPaginas++;
+	};
+
+	$scope.contatos = [];
+	for(var indice = 0; indice < 1000; indice++){
+		$scope.contatos[indice] = { nome: 'Teste ' + (indice + 1), telefone: '(00) 1234-5678', selecionado: 'N', operadora: { id: 1, nome: 'VIVO' }, dataCadastro: new Date() }
+	}
 
 	$scope.operadoras = [
 		{ id: 1, nome: 'VIVO'},
@@ -22,43 +43,54 @@ app.controller('ListaTelefonicaController', function($scope){
     	{id:2, sigla:'F', descricao:'Feminino'}
     ];
 
-	$scope.existemContatos = function(){
+	$scope.existemContatos = function () {
 		return $scope.contatos != undefined && $scope.contatos.length > 0;
 	};
 
-	$scope.edicaoAtiva = function(){
+	$scope.edicaoAtiva = function () {
 		return $scope.novo || $scope.contato != undefined && $scope.contato.indice > -1;
 	};
 
-	$scope.camposPreenchidos = function(){
-		return $scope.contato 
-			&& $scope.contato.nome 
-			&& $scope.contato.nome.length > 0 
-			&& $scope.contato.telefone
-			&& $scope.contato.telefone.length > 0;
-
-	};
-
-	$scope.salvarContato = function(){
+	$scope.salvarContato = function () {
+		if($scope.formularioCadastro.$invalid){
+			$scope.formularioCadastro.$setDirty();
+			for(var propName in $scope.formularioCadastro){
+				var prop = $scope.formularioCadastro[propName];
+				if(prop.$setViewValue != undefined){
+					prop.$setViewValue(prop.$viewValue);
+				}
+			}
+			return;
+		}
 		var contato = $scope.contato;
 		if(contato == undefined) return;
 		if(contato.indice != undefined && contato.indice > -1){
+			contato.dataAtualizacao = new Date();
 			$scope.contatos[contato.indice] = contato;
 		}else{
+			contato.dataCadastro = new Date();
 			$scope.contatos.push(contato);
 		}
-		delete $scope.contato;
-		delete $scope.novo;
+		$scope.cancelarEdicaoContato();
 	};
 
-	$scope.editarContato = function(indice, contato){
+	$scope.editarContato = function (indice, contato) {
 		contato.indice = indice;
 		$scope.contato = {};
 		angular.extend($scope.contato, contato);
 	};
 
-	$scope.cancelarEdicaoContato = function(){
+	$scope.cancelarEdicaoContato = function () {
 		delete $scope.contato;
 		delete $scope.novo;
-	};		
-});
+		$scope.formularioCadastro.$setPristine();
+	};
+
+	function init(){
+		$interval(function () {
+			$scope.agora = new Date();
+		}, 1000);
+	}
+
+	init();
+}]);
